5af802f77f364297e03f51559dcac23602758ea3
kkomaz <leeajl006@gmail.com>
2016-04-29 16:01:57 -0400

    Generate proper response on POST

--- src/containers/registration_form.jsx
+++ src/containers/registration_form.jsx
@@ -3,7 +3,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { registerEmail } from '../actions/index';
 
-const { func } = React.PropTypes;
+const { func, obj } = React.PropTypes;
 
 class RegistrationForm extends Component {
   constructor(props) {
@@ -14,11 +14,12 @@ class RegistrationForm extends Component {
     this.onInputChange = this.onInputChange.bind(this);
     this.onBlurChange = this.onBlurChange.bind(this);
   }
+
   onFormSubmit(event) {
     event.preventDefault();
 
     this.props.registerEmail(this.state);
-    this.setState({ email: '', buttonSubmit: 'inactive' });
+    this.setState({ email: '' });
   }
 
   onInputChange(event) {
@@ -29,12 +30,35 @@ class RegistrationForm extends Component {
     this.setState({ [event.target.id]: event.target.value });
   }
 
-  renderSubmit() {
+  renderSubmitButton() {
     if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(this.state.email)) {
-      return <button type="submit" disabled="disabled" className="button button--inactive">Submit</button>;
-    } else {
-      return <button type="submit" className="button button--active">Submit</button>;
+      return <button type="submit" className="button button--inactive">Submit</button>;
     }
+
+    return <button type="submit" className="button button--active">Submit</button>;
+  }
+
+  renderErrors() {
+    const { response } = this.props;
+
+    return response.data.errors.map((error) => {
+      return <li>{error}</li>;
+    });
+  }
+
+  renderResponse() {
+    const { response } = this.props;
+
+    if (response.status && response.status === 201) {
+      return <p>Thank you for subscribing!  You should receieve an email shortly!</p>;
+    }
+
+    if (response.status) {
+      let errors = this.renderErrors();
+      return <ul>{errors}</ul>;
+    }
+
+    return null;
   }
 
   render() {
@@ -53,9 +77,11 @@ class RegistrationForm extends Component {
         />
 
         <div className="registration-form__submit">
-            {this.renderSubmit()}
+          {this.renderSubmitButton()}
         </div>
         </form>
+
+        {this.renderResponse()}
       </div>
     );
   }
@@ -63,6 +89,7 @@ class RegistrationForm extends Component {
 
 RegistrationForm.propTypes = {
   registerEmail: func,
+  response: obj,
 };
 
 function mapStateToProps(state) {
